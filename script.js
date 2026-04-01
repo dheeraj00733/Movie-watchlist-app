df14e337ed65622a2ab1d81e2803c95a02597fe6
Raghav <dheeraj62844@gmail.com>
2026-04-01 17:59:40 +0530

    Code for project

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const API_KEY = "460a15d4";
+
+
+async function searchMovies() {
+  const query = document.getElementById("searchInput").value;
+
+  if (!query) return;
+
+  const loading = document.getElementById("loading");
+  const container = document.getElementById("moviesContainer");
+
+  loading.style.display = "block";
+  container.innerHTML = "" ;
+
+  
