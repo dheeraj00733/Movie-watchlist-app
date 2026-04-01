0335015fec6a425810a69a42b37a19c4581e71b7
Raghav <dheeraj62844@gmail.com>
2026-04-01 18:19:00 +0530

    Movies data

--- script.js
+++ script.js
@@ -12,4 +12,28 @@ async function searchMovies() {
   loading.style.display = "block";
   container.innerHTML = "" ;
 
-  
+  try {
+    const res = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=460a15d4`);
+    const data = await res.json()
+    loading.style.display = "none";
+
+    if (data.search){
+        displayMovies(data.search)
+    }else{
+        container.innerHTML = "<p>No movies found</p>" ;
+    }
+  }catch(error){
+    loading.style.display = "none";
+    console.log(error)
+  }
+}
+
+function displayMovies(movies){
+    const container = document.getElementById("moviesContainer")
+    movies.forEach(movie => {
+        const div = document.createElement("div");
+
+    div.innerHTML = 
+    <h3>${movie.Title}</h3>
+    })
+}
